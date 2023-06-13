7d64e1dec6c900dcd4c7df0dca1d216bfc525b6e
iairkap <iairkap@gmail.com>
2023-06-13 17:51:01 -0300

    feat/front/iair jobs modularizado + view jobs

---
+++ components/jobsComponents/JobsOfferCardsComponents/JobsOfferCardsContainerForHomePage/JobsOfferCardsContainerForHomePage.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import styles from "../JobsOfferCardsContainer/JobsOfferCardsContainer.module.css";
+import JobsOfferCard from "../JobsOffer Card/JobsOfferCard";
+import formatDate from "../../../helpers/formatDate";
+import JobsOfferDetail from "../../JobsOfferDetail/JobsOfferDetail";
+
+const JobsOfferCardsContainerForHome = ({ jobs }) => {
+  const [selectedJobId, setSelectedJobId] = useState(null);
+
+  const onJobSelected = (id) => {
+    setSelectedJobId(id);
+  };
+
+  return (
+    <div className={styles.forajido}>
+      <div
+        style={{
+          maxHeight: "572px",
+          overflowY: "auto",
+          display: "flex", // Asegura que el contenedor se comporte como una columna
+        }}
+      >
+        <ul
+          style={{
+            flex: "1", // Permite que la lista crezca y ocupe todo el espacio restante
+            color: "gray",
+            textShadow: "white",
+          }}
+        >
+          {jobs.map((job, index) => {
+            const formatStart = formatDate(job.start); // Llama a formatDate aquí
+            return (
+              <JobsOfferCard
+                key={index}
+                id={job.id}
+                company={job.company}
+                logo_Company={job.logo_Company}
+                name_Vacancy={job.name_Vacancy}
+                seniority={job.seniority}
+                start={formatStart}
+                showSpan={true}
+                onJobSelected={onJobSelected}
+              />
+            );
+          })}
+        </ul>
+      </div>
+      <JobsOfferDetail selectedJobId={selectedJobId} />
+    </div>
+  );
+};
+
+export default JobsOfferCardsContainerForHome;
