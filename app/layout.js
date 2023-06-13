b0438f7a204030e0ec9af7d76a04cedfa93b5ce3
iairkap <iairkap@gmail.com>
2023-06-13 19:31:39 -0300

    fix

--- app/layout.js
+++ app/layout.js
@@ -12,8 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <NavBar />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NavBar />
+        {children}
+      </body>
     </html>
   );
 }
