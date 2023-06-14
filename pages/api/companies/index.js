d53ba6be7153b2bcb356d13f45712d107c6bd85d
ignaciocenni <ignaziocenni@gmail.com>
2023-06-14 14:16:16 -0300

    Dejo un comentario en el archivo pages/api/companies/index

--- pages/api/companies/index.js
+++ pages/api/companies/index.js
@@ -1,26 +1,28 @@
 const handler = async (req, res) => {
-    if (req.method === 'POST') {
-        const newCompany = await fetch('http://localhost:3001/data', {
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            method: 'POST',
-            body: JSON.stringify(req.body)
-        })
+  if (req.method === "POST") {
+    const newCompany = await fetch("http://localhost:3001/data", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(req.body),
+    });
 
-        return res.status(201).json(newCompany)
-    }
+    return res.status(201).json(newCompany);
+  }
 
-    if (req.method === 'GET') {
-        const response = await fetch('http://localhost:3001/data', {
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
-        const companies = await response.json()
-        return res.status(200).json(companies)
-    };
-    return res.status(400).json({ error: 'el método no existe' })
-}
+  if (req.method === "GET") {
+    const response = await fetch("http://localhost:3001/data", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const companies = await response.json();
+    return res.status(200).json(companies);
+  }
+  return res.status(400).json({ error: "el método no existe" });
+};
 
 export default handler;
+
+// deberíamos utilizar la carpeta api dentro de app, estamos usando el app router, no el page router
