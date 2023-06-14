45d78d51ec708877fe5617ca2f8c5ec2bbcbbe12
Natalia Malvicino <natalia.malvicino@gmail.com>
2023-06-14 00:34:08 -0300

    feat/front/nati-signup new page

---
+++ app/profile/signup/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./signup.module.css";
+
+function SignUp() {
+  return (
+    <div>
+      <h1 className={styles.title}>Sign up to Tech Talent Hub</h1>
+      <h3 className={styles.subtitle}>
+        Ready to find your dream job? Discover companies from all over Israel
+      </h3>
+    </div>
+  );
+}
+
+export default SignUp;
