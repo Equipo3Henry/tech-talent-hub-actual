d19096ac8b23a8b7da8ffe83c53fa2d3dd5490df
iairkap <iairkap@gmail.com>
2023-06-13 22:53:31 -0300

    falta el tercer navbar

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -19,60 +19,85 @@ import Link from "next/link";
 const NavBar = ({ handleBotonMenu }) => {
   const [isProfile, setIsProfile] = useState(false);
   const [isCompany, setIsCompany] = useState(false);
+  const [isPreLogIn, setIsPreLogIn] = useState(false);
 
   const router = usePathname();
 
   useEffect(() => {
     if (router.includes("profile")) {
       setIsProfile(true);
+      setIsCompany(false);
+      setIsPreLogIn(false);
+    } else if (router.includes("company")) {
+      setIsProfile(false);
+      setIsCompany(true);
+      setIsPreLogIn(false);
     } else {
       setIsProfile(false);
+      setIsCompany(false);
+      setIsPreLogIn(true);
     }
   }, [router]);
 
   console.log(router);
 
-  return isProfile ? (
-    <div className={styles.Navbar}>
+  if (isProfile) {
+    return (
+      <div className={styles.Navbar}>
+        <div className={styles.MenuContainer} onClick={handleBotonMenu}>
+          <Image src={Vector} alt="image" className={styles.Menu} />
+        </div>
+        <div className={styles.IconsContainer}>
+          <Link href="/profile/dashboard">
+            <Image src={dashboard} alt="image" className={styles.Icons} />
+          </Link>
+          <Link href="/profile/my-applications">
+            <Image src={posts} alt="image" className={styles.Icons} />
+          </Link>
+          <Link href="/profile/notifications">
+            <Image src={notification} alt="image" className={styles.Icons} />
+          </Link>
+          <Link href="/profile/favorites">
+            <Image src={favorites} alt="image" className={styles.Icons} />
+          </Link>
+        </div>
+      </div>
+    );
+  } else if (isCompany) {
+    return (
+      <div className={styles.NavbarCompanies}>
+        <div className={styles.MenuContainer} onClick={handleBotonMenu}>
+          <Image src={Vector} alt="image" className={styles.Menu} />
+        </div>
+        <div className={styles.IconsContainer}>
+          <Link href="/company/dashboard">
+            <Image src={dashboard} alt="image" className={styles.Icons} />
+          </Link>
+          <Link href="/company/my-posts">
+            <Image src={posts} alt="image" className={styles.Icons} />
+          </Link>
+          <Link href="/company/notifications">
+            <Image src={notification} alt="image" className={styles.Icons} />
+          </Link>
+          <Link href="/company/favorites">
+            <Image src={favorites} alt="image" className={styles.Icons} />{" "}
+          </Link>
+        </div>
+      </div>
+    );
+    /*  } else if (isPreLogIn){
+    return (
+      <div className={styles.Navbar}>
       <div className={styles.MenuContainer} onClick={handleBotonMenu}>
         <Image src={Vector} alt="image" className={styles.Menu} />
       </div>
       <div className={styles.IconsContainer}>
-        <Link href="/profile/dashboard">
-          <Image src={dashboard} alt="image" className={styles.Icons} />
+        <Link href="/home">
+          <Image src={home} alt="image" className={styles.Icons} />
         </Link>
-        <Link href="/profile/my-applications">
-          <Image src={posts} alt="image" className={styles.Icons} />
-        </Link>
-        <Link href="/profile/notifications">
-          <Image src={notification} alt="image" className={styles.Icons} />
-        </Link>
-        <Link href="/profile/favorites">
-          <Image src={favorites} alt="image" className={styles.Icons} />
-        </Link>
-      </div>
-    </div>
-  ) : (
-    <div className={styles.NavbarCompanies}>
-      <div className={styles.MenuContainer} onClick={handleBotonMenu}>
-        <Image src={Vector} alt="image" className={styles.Menu} />
-      </div>
-      <div className={styles.IconsContainer}>
-        <Link href="/company/dashboard">
-          <Image src={dashboard} alt="image" className={styles.Icons} />
-        </Link>
-        <Link href="/company/my-posts">
-          <Image src={posts} alt="image" className={styles.Icons} />
-        </Link>
-        <Link href="/company/notifications">
-          <Image src={notification} alt="image" className={styles.Icons} />
-        </Link>
-        <Link href="/company/favorites">
-          <Image src={favorites} alt="image" className={styles.Icons} />{" "}
-        </Link>
-      </div>
-    </div>
-  );
+        <Link href="/about-us">
+          <Image stc = {} */
+  }
 };
 
 export default NavBar;
