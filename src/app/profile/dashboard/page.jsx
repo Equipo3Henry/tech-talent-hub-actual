2252d0bb9f32270fbed5505fe4ed1521efd9d658
Blessed0314 <christian.daniel999@gmail.com>
2023-06-15 15:17:29 -0500

    set values for filter selects

--- src/app/profile/dashboard/page.jsx
+++ src/app/profile/dashboard/page.jsx
@@ -4,23 +4,31 @@ import JobsOfferCardsContainerForHome from "../../components/jobsComponents/Jobs
 import styles from "./homePage.module.css";
 import { jobsTemplate } from "../../helpers/provisionalDB";
 import JobsOfferDetail from "../../components/jobsComponents/JobsOfferDetail/JobsOfferDetail";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import SelectsContainer from "../../components/generalComponents/selectComponent/SelectContainer/SelectContainer";
+
 
 const jobs = jobsTemplate;
 function homePage() {
+
   const [selectedJob, setSelectedJob] = useState(null);
+  const selectedValuesRef = useRef({});
 
   const handleJobSelect = (jobId) => {
     const jobDetail = jobs.find((job) => job.id === jobId);
     setSelectedJob(jobDetail);
   };
 
+  const handleSelectChange = (values) => {
+    selectedValuesRef.current = values;
+    console.log(selectedValuesRef.current);
+  };
+  
   return (
     <div className={styles.globalContainer}>
       <SearchBar />
       <br />
-      <SelectsContainer />
+      <SelectsContainer onSelectChange={handleSelectChange} />
       <br />
       <div className={styles.forniculo}>
         <div className={styles.jobsContainer}>
