f7ca6f00aa3a070b7626fd8c27947341fc2bb258
Blessed0314 <christian.daniel999@gmail.com>
2023-06-15 15:17:29 -0500

    set values for filter selects

--- src/app/components/generalComponents/selectComponent/Select/Select.jsx
+++ src/app/components/generalComponents/selectComponent/Select/Select.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./Select.module.css";
 
-function Select({ options, name }) {
+function Select({ options, name, onChange, defaultValue }) {
+  const selectRef = useRef(null);
+
+  const handleSelectChange = () => {
+    const selectedValue = selectRef.current.value;
+    onChange(name, selectedValue);
+  };
+
+  
+
   return (
     <div>
-      <select name={name} id={name} className={styles.select}>
+      <select name={name} id={name} className={styles.select} ref={selectRef} onChange={handleSelectChange} defaultValue={defaultValue} >
         {options.map((option) => (
-          <option value={option.id} key={option.id} className={styles.value}>
+          <option value={option.name} key={option.id} className={styles.value}>
             {option.name}
           </option>
         ))}
--- src/app/components/generalComponents/selectComponent/SelectContainer/SelectContainer.jsx
+++ src/app/components/generalComponents/selectComponent/SelectContainer/SelectContainer.jsx
@@ -1,12 +1,35 @@
 import filtersDB from "../../../../helpers/provisionalFiltersDB";
 import Select from "../Select/Select";
 import styles from "./SelectContainer.module.css";
+import React, { useRef, useEffect } from "react";
 
-const SelectsContainer = () => {
+const SelectsContainer = ({ onSelectChange }) => {
+  
+  const selectedValuesRef = useRef({});
+
+  const handleSelectChange = (name, value) => {
+    selectedValuesRef.current[name] = value;
+    onSelectChange(selectedValuesRef.current);
+    
+  };
+
+  useEffect(() => {
+    // Obtener los valores seleccionados al montar el componente
+    Object.keys(filtersDB).forEach((key) => {
+      const selectedValue = selectedValuesRef.current[key];
+      if (selectedValue) {
+        onSelectChange(selectedValuesRef.current);
+        return;
+      }
+      const defaultValue = filtersDB[key][0].name;
+      selectedValuesRef.current[key] = defaultValue;
+    });
+  }, []);
+
   return (
     <div className={styles.Container}>
       {Object.keys(filtersDB).map((key) => (
-        <Select key={key} name={key} options={filtersDB[key]} />
+        <Select key={key} name={key} options={filtersDB[key]} onChange={handleSelectChange} defaultValue={selectedValuesRef.current[key]}/>
       ))}
     </div>
   );
