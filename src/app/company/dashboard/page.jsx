97046bc8defe29b6dfd223fd31273437a32be6da
Blessed0314 <christian.daniel999@gmail.com>
2023-06-15 16:06:27 -0500

    Se aplica lo mismo para company

--- src/app/company/dashboard/page.jsx
+++ src/app/company/dashboard/page.jsx
@@ -3,23 +3,31 @@ import SearchBar from "@/src/app/components/generalComponents/SearchBar/searchBa
 import UserOfferCardsContainerForDashboard from "@/src/app/components/usersComponents/UserOfferComponents/UserOffereCardsContainer/UserOfferCardsContainer";
 import styles from "./dashboard.module.css";
 import { usersTemplate } from "@/src/app/helpers/provisionalDB";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import SelectsContainer from "@/src/app/components/generalComponents/selectComponent/SelectContainer/SelectContainer";
 
 const users = usersTemplate;
 
 function dashboardPage() {
+
   const [selectedUser, setSelectedUser] = useState(null);
+  const selectedValuesRef = useRef({});
 
   const handleUserSelect = (userId) => {
     const userDetail = users.find((user) => user.id === userId);
     setSelectedUser(userDetail);
   };
+
+  const handleSelectChange = (values) => {
+    selectedValuesRef.current = values;
+    console.log(selectedValuesRef.current);
+  };
+
   return (
     <div className={styles.globalContainer}>
       <SearchBar />
       <br />
-      <SelectsContainer />
+      <SelectsContainer  onSelectChange={handleSelectChange}/>
       <br />
       <div className={styles.forniculo}>
         <div className={styles.usersContainer}>
