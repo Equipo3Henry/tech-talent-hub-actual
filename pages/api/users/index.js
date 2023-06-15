c782127ae9100c66b7ce49830b90b97016d5ab23
Iván Scarsella <scarsellaivan@gmail.com>
2023-06-15 15:01:45 -0300

    hice ruta de users

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,5 +1,55 @@
+import prisma from "@/prisma/client";
+
 export default async function handler(req, res) {
-  if (req.method == "GET") {
-    res.json({ message: "Hello users" });
+  if (req.method === "POST") {
+    const {
+      username,
+      name,
+      lastName,
+      birth,
+      aboutMe,
+      working,
+      country,
+      email,
+      password,
+      degree,
+      languages,
+      progLanguages,
+      profile_picture,
+      seniority,
+      cv,
+      softSkills,
+      recruiter,
+    } = req.body;
+
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        name,
+        lastName,
+        birth,
+        aboutMe,
+        working,
+        country,
+        email,
+        password,
+        degree,
+        languages,
+        progLanguages,
+        profile_picture,
+        seniority,
+        cv,
+        softSkills,
+        recruiter,
+      },
+    });
+
+    return res.status(201).json(newUser);
+  }
+
+  if (req.method === "GET") {
+    const allUsers = await prisma.user.findMany();
+
+    return res.status(200).json(allUsers)
   }
 }
