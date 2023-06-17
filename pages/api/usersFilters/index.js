4cb782d2d8e09d59d9c7552bbad3d445d04b51b2
iairkap <iairkap@gmail.com>
2023-06-16 23:56:14 -0300

    fix/filtroscombinados en el front

--- pages/api/usersFilters/index.js
+++ pages/api/usersFilters/index.js
@@ -1,11 +1,11 @@
 import prisma from "@/prisma/client";
 
 export default async function handler(req, res) {
-  const { progLanguage, seniority } = req.query;
+  const { progLanguage, seniority, softSkill } = req.query;
 
   if (req.method === "GET") {
     try {
-      const users = await getUsers(progLanguage, seniority);
+      const users = await getUsers(progLanguage, seniority, softSkill);
       res.status(200).json(users);
     } catch (error) {
       res.status(500).json({ error: "Error retrieving user." });
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
   }
 }
 
-async function getUsers(progLanguage, seniority) {
+async function getUsers(progLanguage, seniority, softSkill) {
   let where = {};
 
   if (progLanguage) {
@@ -28,69 +28,15 @@ async function getUsers(progLanguage, seniority) {
     where.seniority = seniority;
   }
 
+  if (softSkill) {
+    where.softSkills = {
+      has: softSkill,
+    };
+  }
+
   const users = await prisma.user.findMany({
     where,
   });
 
   return users;
 }
-
-/* import prisma from "@/prisma/client";
-
-export default async function handler(req, res) {
-  const { progLanguage, seniority } = req.query;
-
-  if (req.method === "GET") {
-    try {
-      let where = {};
-
-      if (progLanguage) {
-        where.progLanguages = {
-          has: progLanguage,
-        };
-      }
-
-      if (seniority) {
-        where.seniority = seniority;
-      }
-
-      const users = await prisma.user.findMany({
-        where,
-      });
-
-      res.status(200).json(users);
-    } catch (error) {
-      res.status(500).json({ error: "Error retrieving user." });
-    }
-  } else {
-    res.status(400).json({ error: "Invalid method." });
-  }
-} */
-
-/* import prisma from "@/prisma/client";
-
-export default async function handler(req, res) {
-  const { progLanguage, seniority } = req.query;
-
-  if (req.method === "GET") {
-    try {
-      let users = await prisma.user.findMany();
-
-      if (progLanguage) {
-        users = users.where((user) =>
-          user.progLanguages.includes(progLanguage)
-        );
-      }
-
-      if (seniority) {
-        users = users.where((user) => user.seniority === seniority);
-      }
-      res.status(200).json(users);
-    } catch (error) {
-      res.status(500).json({ error: "Error retrieving user." });
-    }
-  } else {
-    res.status(400).json({ error: "Invalid method." });
-  }
-}
- */
