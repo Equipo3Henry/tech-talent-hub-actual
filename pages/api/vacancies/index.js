b15d4b787cea5378b7df0b73e935f1105edf2b43
iairkap <iairkap@gmail.com>
2023-06-17 09:50:54 -0300

    agregue para que se puedan hacer mulples post de vacancies

--- pages/api/vacancies/index.js
+++ pages/api/vacancies/index.js
@@ -2,7 +2,21 @@ import prisma from "@/prisma/client";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    try {
+    if (Array.isArray(req.body)) {
+      const vacancies = req.body;
+
+      try {
+        await prisma.vacancy.createMany({
+          data: vacancies,
+        });
+
+        return res
+          .status(201)
+          .json({ message: "Vacancies created successfully" });
+      } catch (error) {
+        return res.status(400).json({ error: error.message });
+      }
+    } else {
       const {
         name_Vacancy,
         company,
@@ -18,37 +32,40 @@ export default async function handler(req, res) {
         Relevance,
       } = req.body;
 
-      const newVacancy = await prisma.Vacancy.create({
-        data: {
-          name_Vacancy,
-          company,
-          logo_Company,
-          programming_Languages,
-          seniority,
-          years_of_experience,
-          description,
-          workday,
-          salary,
-          date_Hire,
-          isActive,
-          Relevance,
-        },
-      });
+      try {
+        const newVacancy = await prisma.vacancy.create({
+          data: {
+            name_Vacancy,
+            company,
+            logo_Company,
+            programming_Languages,
+            seniority,
+            years_of_experience,
+            description,
+            workday,
+            salary,
+            date_Hire,
+            isActive,
+            Relevance,
+          },
+        });
 
-      return res.status(201).json(newVacancy);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Error creating vacancy" });
+        return res.status(201).json(newVacancy);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Error creating vacancy" });
+      }
     }
   }
 
   if (req.method === "GET") {
     try {
-      const allVacancies = await prisma.Vacancy.findMany();
-      return res.status(200).json(allVacancies);
+      const vacancies = await prisma.vacancy.findMany();
+
+      return res.status(200).json(vacancies);
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: error.message });
+      res.status(500).json({ error: "Error getting vacancies" });
     }
   }
 }
