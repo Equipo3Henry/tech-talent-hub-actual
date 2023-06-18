33764f0f70b72b1740ad1d800ac3775bd4b491b6
iairkap <iairkap@gmail.com>
2023-06-18 16:18:35 -0300

    searchbar company font

--- src/app/components/generalComponents/SearchBar/searchBar.jsx
+++ src/app/components/generalComponents/SearchBar/searchBar.jsx
@@ -1,4 +1,4 @@
-"use client";
+/* "use client";
 
 import styles from "./searchBar.module.css";
 import search from "../../../public/images/search.svg";
@@ -27,6 +27,44 @@ function SearchBar() {
           <Image src={search} alt="image" className={styles.Icons} />
         </button>
       </div>
+    </div>
+  );
+}
+
+export default SearchBar;
+ */
+
+import { useState } from "react";
+import styles from "./searchBar.module.css";
+import search from "../../../public/images/search.svg";
+import Image from "next/image";
+
+function SearchBar({ onSearch }) {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+
+    // Llama a la función de búsqueda que se pasó como un prop
+    onSearch(searchValue);
+  };
+
+  return (
+    <div className={styles.searchBarContainer}>
+      <form onSubmit={handleSearch}>
+        <div className={styles.searchBar}>
+          <input
+            type="search"
+            placeholder="Search for job titles, companies or keywords"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            className={styles.input}
+          />
+          <button type="submit" className={styles.button}>
+            <Image src={search} alt="image" className={styles.Icons} />
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
