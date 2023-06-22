8d7738eec09e9067c5c7b1535d8c208c75ac5e2e
iairkap <iairkap@gmail.com>
2023-06-22 18:19:13 -0300

    feat/local-storage-companies

--- src/app/company/dashboard/page.jsx
+++ src/app/company/dashboard/page.jsx
@@ -1,21 +1,35 @@
 "use client";
-import React, { useState, useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import SearchBar from "@/src/app/components/generalComponents/SearchBar/searchBar";
 import UserOfferCardsContainerForDashboard from "@/src/app/components/usersComponents/UserOfferComponents/UserOffereCardsContainer/UserOfferCardsContainer";
 import styles from "./dashboard.module.css";
 import FiltersSelector from "../../components/SelectorFiltersForCompanyDashboard/filtrosCombinados";
 import { GlobalContext } from "../layout";
 import { getLayout } from "../layout";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
 
 function DashboardPage() {
   const {
     users,
+    compData,
     setSelectedProgLanguage,
     setSelectedSeniority,
     setSelectedSoftSkill,
     setSearchValue,
+    setCompanies,
   } = useContext(GlobalContext);
 
+  const params = useSearchParams();
+  const companyData = JSON.parse(params.get("companyData"));
+
+  useEffect(() => {
+    const localStorageData = localStorage.getItem("companyData");
+    const companyData = JSON.parse(localStorageData);
+    setCompanies(companyData);
+  }, []); // Dependency array
+
+  console.log(compData);
   return (
     <div className={styles.globalContainer}>
       <SearchBar setSearchValue={setSearchValue} />
