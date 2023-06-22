4c610a395bcc72d8035b1752e9441dcb9cdb7de7
iairkap <iairkap@gmail.com>
2023-06-22 19:31:50 -0300

    cloudinary

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
   },
   env: {
     URL_DEPLOY: process.env.URL_DEPLOY,
+    CLOUD_NAME: process.env.CLOUD_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+    CLOUDINARY_UPLOAD_PRESET: process.env.CLOUDINARY_UPLOAD_PRESET,
   },
 };
 
