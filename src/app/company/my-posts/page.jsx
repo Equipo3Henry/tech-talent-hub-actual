c3ddcfceba987f745e31189a3626a6b4981af067
iairkap <iairkap@gmail.com>
2023-06-23 12:12:43 -0300

    gracias cris

--- src/app/company/my-posts/page.jsx
+++ src/app/company/my-posts/page.jsx
@@ -7,19 +7,28 @@ import Image from "next/image";
 import MyPostsCards from "../../components/jobsComponents/jobsOfferCardContainerForMyPosts/myPosts";
 
 function MyPosts(props) {
-  const localStorageData = localStorage.getItem("companyData");
-  console.log("localStorageData:", localStorageData);
+  const [companyData, setCompanyData] = useState(null);
+  const [companyId, setCompanyId] = useState(null);
+  const [companyPicture, setCompanyPicture] = useState(null);
 
-  const companyData = JSON.parse(localStorageData);
-  console.log("companyData:", companyData);
+  useEffect(() => {
+    const localStorageData = localStorage.getItem("companyData");
+    console.log("localStorageData:", localStorageData);
+    const parsedData = localStorageData ? JSON.parse(localStorageData) : null;
+    console.log("parsedData:", parsedData);
 
-  const companyId = companyData.id;
-  const companyPicture = companyData && companyData.logo_Company;
+    if (parsedData) {
+      setCompanyData(parsedData);
+      setCompanyId(parsedData.id);
+      setCompanyPicture(parsedData.logo_Company);
+    }
+  }, []);
+
   return (
     <>
       <div className={styles.page_container}>
         <div className={styles.posts_container}>
-          <MyPostsCards />
+          <MyPostsCards companyId={companyId} />
         </div>
         <h1>My Posts</h1>
 
