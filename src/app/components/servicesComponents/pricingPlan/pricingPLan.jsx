086a81f915de0ef0828333bcded5cf68fe4936ff
Iván Scarsella <scarsellaivan@gmail.com>
2023-06-23 16:31:03 -0300

    fix mercado pago

--- src/app/components/servicesComponents/pricingPlan/pricingPLan.jsx
+++ src/app/components/servicesComponents/pricingPlan/pricingPLan.jsx
@@ -1,9 +1,43 @@
 import { plans } from "@/src/mock/plans";
-import React from "react";
+import React, { useEffect } from "react";
 import { MercadoPagoButton } from "../../MercadoPagoButton/MercadoPagoButton";
 import styles from "./pricingPlan.module.css";
 
 const PricingPlan = ({ plan, borderColor }) => {
+
+  const [notification, setNotification] = useState({
+    isOpen: false,
+    type: null,
+    content: ""
+  });
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const status = urlParams.get("status")
+
+    if (status === "approved") {
+      setNotification({
+        content: "El pago se realizó correctamente",
+        isOpen: true,
+        type: "approved"
+      })
+    } else if (status === "failure") {
+      setNotification({
+        content: "Hubo un error al realizar el pago",
+        isOpen: true,
+        type: "failure"
+      })
+    }
+//hasdohfsdohf
+    setTimeout(() => {
+      setNotification({
+        isOpen: false,
+        type: null,
+        content: ""
+      })
+    }, 5000)
+  }, [])
+
   return (
     <div>
       <div className={styles.Contenedor} style={{ borderColor: borderColor }}>
@@ -20,6 +54,13 @@ const PricingPlan = ({ plan, borderColor }) => {
           ))}
         </ul>
       </div>
+      {notification.isOpen && (
+        <div className={styles.notification}>
+          <div className={styles.iconContainer} style={{ background: notification.type === "approved" ? "#00cc99" : "#ee4646" }}>
+            <Image src={`/assets/${notification.type}.svg`} alt={notification.type} width={25} height={25} />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
