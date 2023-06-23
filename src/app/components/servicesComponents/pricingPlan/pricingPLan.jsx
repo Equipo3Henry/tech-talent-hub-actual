b659a770836d05f5a6bdf93c70dfc363b598ac7f
Iván Scarsella <scarsellaivan@gmail.com>
2023-06-23 10:19:26 -0300

    agregué función de marcadopago

--- src/app/components/servicesComponents/pricingPlan/pricingPLan.jsx
+++ src/app/components/servicesComponents/pricingPlan/pricingPLan.jsx
@@ -1,4 +1,6 @@
+import { plans } from "@/src/mock/plans";
 import React from "react";
+import { MercadoPagoButton } from "../../MercadoPagoButton/MercadoPagoButton";
 import styles from "./pricingPlan.module.css";
 
 const PricingPlan = ({ plan, borderColor }) => {
@@ -7,8 +9,9 @@ const PricingPlan = ({ plan, borderColor }) => {
       <div className={styles.Contenedor} style={{ borderColor: borderColor }}>
         <h1 className={styles.title}>{plan.title}</h1>
         <h3 className={styles.subtitles}>{plan.subtitles}</h3>
-        <h1 className={styles.prices}>{plan.price}</h1>
-        <button className={styles.button}>Join now</button>
+        <h1 className={styles.prices}>{plan.priceString}</h1>
+        <MercadoPagoButton plan={plan} />
+        {/* <button className={styles.button}>Join now</button> */}
         <ul>
           {plan.features.map((feature, index) => (
             <li className={styles.list} key={index}>
---
+++ src/mock/plans.js
@@ -0,0 +1,34 @@
+export const plans = [
+  {
+    title: "Option 1",
+    subtitles: "Lorem ipsum dolor sit amet",
+    price: 0,
+    priceString: "Free",
+    features: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    ],
+  },
+  {
+    title: "Option 2",
+    subtitles: "Lorem ipsum dolor sit amet",
+    price: 15,
+    priceString: "$15",
+    features: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    ],
+  },
+  // {
+  //   title: "Option 3",
+  //   subtitles: "Lorem ipsum dolor sit amet",
+  //   price: "$35",
+  //   features: [
+  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  //   ],
+  // },
+];
