4fa5adb01997463708ad4491a4f950cb7f77b873
iairkap <iairkap@gmail.com>
2023-06-23 12:50:45 -0300

    put-backend

--- pages/api/companies/[id]/index.js
+++ pages/api/companies/[id]/index.js
@@ -1,4 +1,6 @@
 import prisma from "@/prisma/client";
+import transporter from "../../sendEmail/index";
+import { encrypt } from "../../helpers/handleBcrypt";
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
@@ -7,8 +9,8 @@ export default async function handler(req, res) {
     try {
       const company = await prisma.company.findUnique({
         where: {
-          id: id
-        }
+          id: id,
+        },
       });
 
       if (!company) {
@@ -18,6 +20,60 @@ export default async function handler(req, res) {
       return res.status(200).json(company);
     } catch (error) {
       console.error("Error retrieving company:", error);
+      return res.status(500).json({ error: error.message });
+    }
+  } else if (req.method === "PUT") {
+    const { id } = req.query;
+
+    try {
+      const {
+        name,
+        logo_Company,
+        type,
+        email,
+        password, // remember to hash this before saving
+        country,
+        description,
+        employes,
+      } = req.body;
+
+      const companyEmail = email;
+      const encryptPass = await encrypt(password);
+
+      const updatedCompany = await prisma.company.update({
+        where: {
+          id: id,
+        },
+        data: {
+          name,
+          logo_Company,
+          type,
+          email,
+          password: encryptPass,
+          country,
+          description,
+          employes,
+        },
+      });
+
+      // Send the email confirmation after the update
+      await transporter.verify();
+      const mail = {
+        from: "equipo3.37a@gmail.com",
+        to: companyEmail,
+        subject: "Registro actualizado",
+        html: `
+        <p style="color: black">
+        Su registro ha sido actualizado ${email}
+        </p>
+        `,
+      };
+      console.log(mail);
+      await transporter.sendMail(mail);
+
+      return res.status(200).json(updatedCompany);
+    } catch (error) {
+      console.error("Error updating company:", error);
       return res.status(500).json({ error: error.message });
     }
   } else {
