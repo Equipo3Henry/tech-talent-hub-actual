809cae2cda81b670a2da2d375a4213a342116078
iairkap <iairkap@gmail.com>
2023-06-23 11:57:10 -0300

    local-storage

--- src/app/components/jobsComponents/jobsOfferCardContainerForMyPosts/myPosts.jsx
+++ src/app/components/jobsComponents/jobsOfferCardContainerForMyPosts/myPosts.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useState, useContext } from "react";
 import axios from "axios";
 import JobsOfferCard from "../JobsOfferCardsComponents/JobsOffer Card/JobsOfferCard";
@@ -7,33 +8,35 @@ const MyPostsCards = () => {
   const [jobs, setJobs] = useState([]);
   const { companies } = useContext(GlobalContext);
 
-  // Obteniendo la compañía del localStorage
-  const localStorageData = localStorage.getItem("companyData");
-  console.log("localStorageData:", localStorageData);
-
-  const companyData = JSON.parse(localStorageData);
-  console.log("companyData:", companyData);
-
-  const companyId = companyData.id; // Aquí obtenemos el ID de la compañía
-
   useEffect(() => {
-    axios.get("/api/vacancies").then((response) => {
-      const jobsFromServer = response.data;
-      const filteredJobs = jobsFromServer.filter(
-        (job) => job.companyId === companyId // Aquí comparamos con el ID de la compañía
-      );
-      setJobs(filteredJobs);
-    });
+    // Obteniendo la compañía del localStorage si está disponible
+    const localStorageData =
+      typeof localStorage !== "undefined"
+        ? localStorage.getItem("companyData")
+        : null;
+    const companyData = localStorageData ? JSON.parse(localStorageData) : null;
+    const companyId = companyData ? companyData.id : null;
+
+    if (companyId) {
+      axios.get("/api/vacancies").then((response) => {
+        const jobsFromServer = response.data;
+        const filteredJobs = jobsFromServer.filter(
+          (job) => job.companyId === companyId
+        );
+        setJobs(filteredJobs);
+      });
+    }
   }, [companies]);
 
   const handleFinishProcess = (jobId) => {
-    const status = {isActive: false};
+    const status = { isActive: false };
     const url = `/api/vacancies/${jobId}`;
 
-    console.log(url)
-    axios.put(`${url}`, status)
+    console.log(url);
+    axios
+      .put(`${url}`, status)
       .then((res) => {
-        console.log(res)
+        console.log(res);
         if (res.status === 200) {
           setJobs(
             jobs.map((job) =>
@@ -64,7 +67,7 @@ const MyPostsCards = () => {
             applicants={`${job.applicants.length} candidates applied`}
             status={job.status}
             showFinishButton={true}
-            onFinishProcess={handleFinishProcess} // Pasando la función de manejador de eventos
+            onFinishProcess={handleFinishProcess}
           />
         );
       })}
