c015afc9ca706c416290d039a47029288659cedd
Natalia Malvicino <natalia.malvicino@gmail.com>
2023-06-24 12:09:53 -0300

    fix about project landing

--- src/app/components/aboutProject/aboutProject.jsx
+++ src/app/components/aboutProject/aboutProject.jsx
@@ -6,28 +6,30 @@ import Link from "next/link";
 
 function AboutProject() {
   return (
-    <div className={styles.container}>
-      <Image src={aboutProject} alt="about" className={styles.imagen} />
-      <div className={styles.aboutProject_Container}>
-        <h1 className={styles.h1}>
-          {" "}
-          Bringing Latin American IT Professionals and Israeli Businesses
-          together
-        </h1>
-        <p className={styles.p}>
-          With a vast pool of skilled IT professionals from Latin America and a
-          thriving tech ecosystem in Israel, our platform opens doors to endless
-          possibilities. From startups to established enterprises across various
-          industries, companies can tap into a rich talent pool and discover
-          exceptional individuals who possess the technical expertise, cultural
-          understanding, and innovative mindset required to thrive in the
-          Israeli business landscape. In a rapidly evolving global market, where
-          collaboration and innovation are key, our platform stands at the
-          forefront, facilitating connections that transcend geographical
-          boundaries. Join us today and unlock a world of opportunities where
-          Latin American IT talent meets the dynamic landscape of Israeli
-          companies.
-        </p>
+    <div className={styles.allContainer}>
+      <div className={styles.container}>
+        <Image src={aboutProject} alt="about" className={styles.imagen} />
+        <div className={styles.aboutProject_Container}>
+          <h1 className={styles.h1}>
+            {" "}
+            Bringing Latin American IT Professionals and Israeli Businesses
+            together
+          </h1>
+          <p className={styles.p}>
+            With a vast pool of skilled IT professionals from Latin America and
+            a thriving tech ecosystem in Israel, our platform opens doors to
+            endless possibilities. From startups to established enterprises
+            across various industries, companies can tap into a rich talent pool
+            and discover exceptional individuals who possess the technical
+            expertise, cultural understanding, and innovative mindset required
+            to thrive in the Israeli business landscape. In a rapidly evolving
+            global market, where collaboration and innovation are key, our
+            platform stands at the forefront, facilitating connections that
+            transcend geographical boundaries. Join us today and unlock a world
+            of opportunities where Latin American IT talent meets the dynamic
+            landscape of Israeli companies.
+          </p>
+        </div>
       </div>
     </div>
   );
