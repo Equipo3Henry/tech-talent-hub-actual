b95783619c789ad541004a64a25b7855c26a79bd
iairkap <iairkap@gmail.com>
2023-06-24 19:52:47 -0300

    isPremium

--- pages/api/apply/index.js
+++ pages/api/apply/index.js
@@ -5,20 +5,37 @@ export default async function handler(req, res) {
     const { userId, jobId } = req.body;
 
     try {
-      const result = await prisma.user.update({
+      const user = await prisma.user.findUnique({
         where: { id: userId },
-        data: {
-          appliedVacancies: {
-            connect: { id: jobId },
-          },
-        },
       });
 
-      res.json({
-        success: true,
-        message: "Applied to the job successfully",
-        result,
-      });
+      // Check if user is premium or still has free vacancies left
+      if (user.isPremium || user.limitFreeVacancies > 0) {
+        const result = await prisma.user.update({
+          where: { id: userId },
+          data: {
+            appliedVacancies: {
+              connect: { id: jobId },
+            },
+            // If the user is not premium, decrease their limitFreeVacancies by 1
+            limitFreeVacancies: user.isPremium
+              ? user.limitFreeVacancies
+              : user.limitFreeVacancies - 1,
+          },
+        });
+
+        res.json({
+          success: true,
+          message: "Applied to the job successfully",
+          result,
+        });
+      } else {
+        res.status(403).json({
+          success: false,
+          message:
+            "You've reached the maximum number of free applications this month.",
+        });
+      }
     } catch (error) {
       res.status(500).json({
         success: false,
