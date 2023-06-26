c52000fadd47408c76b2eadc791fb4ada1861d3f
Uruvsereg <uruvsereg@hotmail.com>
2023-06-26 17:22:47 -0500

    Fix page superadmin dash

---
+++ src/app/profile/superAdmin/DashSuperadmin/Elementos/InfoCards.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import style from './InfoCards.module.css';
+//import {dataUsers} from
+
+//import {dataCompanies} from
+//import {dataVacancies} from
+const InfoCard = () =>{
+    return(
+        <div className={style.container}>
+            <div className={style.card}>
+                <div className={style.cardinfo}>
+                    <p className={style.grade}>Basic Users</p>{/*Aquí se podría cambiar por el */}
+                    <p>Free</p>
+                </div>
+                <p className={style.containerlot}>
+                    <span className={style.lot}>10</span>
+                </p>
+            </div>
+            <div className={style.card}>
+                <div className={style.cardinfo}>
+                    <p className={style.grade}>Premium Users</p>
+                    <p>20 USD</p>
+                </div>
+                <p className={style.containerlot}>
+                    <span className={style.lot}>3</span>
+                </p>
+            </div>
+            <div className={style.card}>
+                <div className={style.cardinfo}>
+                    <p className={style.grade}>total Users</p>
+                </div>
+                <p className={style.containerlot}>
+                    <span className={style.lot}>13</span>
+                </p>
+            </div>
+        </div>
+    )
+}
+export default InfoCard;
