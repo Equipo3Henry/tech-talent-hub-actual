cb5e4f92845067c23d2d54c16b85f0c0f7045212
iairkap <iairkap@gmail.com>
2023-06-26 22:54:02 -0300

    charts css

--- src/app/profile/superAdmin/page.jsx
+++ src/app/profile/superAdmin/page.jsx
@@ -9,6 +9,7 @@ import InfoCard from "./DashSuperadmin/Elementos/InfoCards";
 import BarcharCompany from "./DashSuperadmin/Graficos/Companies/Barchar";
 import Piechar from "./DashSuperadmin/Graficos/Vacancies/Piechar";
 import { useContext } from "react";
+import styles from "./dashboard.module.css";
 
 function SuperDashboardAdmin() {
   const { jobs, user, companies, allUsers, setAllUsers } =
@@ -39,11 +40,11 @@ function SuperDashboardAdmin() {
   }
 
   return (
-    <div>
+    <div className={styles.contenedor}>
       <Header />
       <InfoCard jobs={jobs} user={user} companies={companies} />
 
-      <div>
+      <div className={styles.containerGraph}>
         <BarcharCompany jobs={jobs} allUsers={allUsers} companies={companies} />
         <Piechar jobs={jobs} user={user} companies={companies} />
         {/*         <Orders />
