230e2e7c3f404e2e7abb218a3f40b4b4cd86e3a5
iairkap <iairkap@gmail.com>
2023-06-26 23:25:16 -0300

    dashboard

--- src/app/profile/superAdmin/DashSuperadmin/Elementos/Header.jsx
+++ src/app/profile/superAdmin/DashSuperadmin/Elementos/Header.jsx
@@ -4,9 +4,7 @@ import style from "./Header.module.css";
 const Header = () => {
   return (
     <div className={style.container}>
-      <h2>Wellcome</h2>
       <h1 className={style.texts}>Dashboard</h1>
-      <h2>Superadmin</h2>
       {/* los punrtos deben ser reemplazados con el nombre de la empresa o usuario */}
     </div>
   );
