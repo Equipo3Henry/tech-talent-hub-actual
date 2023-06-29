3e33acd8af7633d376e453d37d79383dd21d5d63
iairkap <iairkap@gmail.com>
2023-06-29 14:37:54 -0300

    loader en el dashboard y refresh funcionando

--- src/app/profile/superAdmin/page.jsx
+++ src/app/profile/superAdmin/page.jsx
@@ -24,27 +24,31 @@ function SuperDashboardAdmin() {
   const [searchTerm, setSearchTerm] = useState("");
   const [companies, setCompanies] = useState(null);
   const [users, setUsers] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const fetchAllCompanies = async () => {
-    const response = await axios.get("/api/companies?includeInactive=true");
-    setCompanies(response.data);
-  };
   useEffect(() => {
-    fetchAllCompanies();
-  }, []);
+    const fetchAllData = async () => {
+      try {
+        const usersResponse = axios.get("/api/users?includeInactive=true");
+        const companiesResponse = axios.get(
+          "/api/companies?includeInactive=true"
+        );
+        const [usersData, companiesData] = await Promise.all([
+          usersResponse,
+          companiesResponse,
+        ]);
+        setUsers(usersData.data);
+        setCompanies(companiesData.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+        setUsers(null);
+        setCompanies(null);
+        setIsLoading(false);
+      }
+    };
 
-  const fetchAllUsers = async () => {
-    try {
-      const response = await axios.get("/api/users?includeInactive=true");
-      setUsers(response.data);
-    } catch (error) {
-      console.error("Error fetching users: ", error);
-      // You can set users to null or an empty array in case of error if it makes sense for your application
-      setUsers(null);
-    }
-  };
-  useEffect(() => {
-    fetchAllUsers();
+    fetchAllData();
   }, []);
 
   const handleSearchChange = (event) => {
@@ -62,7 +66,7 @@ function SuperDashboardAdmin() {
         company.name.toLowerCase().includes(searchTerm.toLowerCase())
       )
     : [];
-  //? ACORDEON
+
   const [accordionState, setAccordionState] = useState({
     ac1: true,
     ac2: false,
@@ -86,85 +90,99 @@ function SuperDashboardAdmin() {
     }));
   };
 
-  console.log(users);
-
   return (
     <div className={styles.contenedorGeneral}>
       <Header />
       <div className={styles.dataContainer}>
-        <div className={styles.infoContainer}>
-          <InfoCard jobs={jobs} users={users} companies={companies} />
-        </div>
-        <RevenueChart users={users} className={styles.mercadopagoChart} />
-      </div>
-
-      <div className={styles.containerGraphAcc}>
-        <div className={styles.containerGraph}>
-          <BarcharCompany jobs={jobs} users={users} companies={companies} />
-          <Piechar jobs={jobs} users={users} companies={companies} />
-          <CountryPieChart users={users} />
-        </div>
         <div>
-          <input
-            type="text"
-            value={searchTerm}
-            onChange={handleSearchChange}
-            placeholder="Search..."
-            className={styles.buscador}
-          />
-          <section className={styles.ac_container}>
-            <div>
-              <input
-                id="ac-1"
-                name="accordion-1"
-                type="radio"
-                checked={accordionState.ac1}
-                onChange={() => handleAccordionChange("ac1")}
-              />
-              <label htmlFor="ac-1">Users Table</label>
-              <article
-                className={`${styles.ac_small} ${
-                  accordionState.ac1 ? styles.active : ""
-                }`}
-              >
-                <TableUsers users={filteredUsers} />
-              </article>
+          {isLoading ? (
+            <div className={styles.loaderContainer}>
+              <div className={styles.spinner}></div>
             </div>
-            <div>
-              <input
-                id="ac-2"
-                name="accordion-1"
-                type="radio"
-                checked={accordionState.ac2}
-                onChange={() => handleAccordionChange("ac2")}
-              />
-              <label htmlFor="ac-2">Companies Table</label>
-              <article
-                className={`${styles.ac_medium} ${
-                  accordionState.ac2 ? styles.active : ""
-                }`}
-              >
-                <TableCompanies companies={filteredCompanies} />
-              </article>
-            </div>
-            <div>
-              <input
-                id="ac-3"
-                name="accordion-1"
-                type="radio"
-                checked={accordionState.ac3}
-                onChange={() => handleAccordionChange("ac3")}
-              />
-              <label htmlFor="ac-3">Vacancies Table</label>
-              <article
-                className={`${styles.ac_large} ${
-                  accordionState.ac3 ? styles.active : ""
-                }`}
-              >
-                <TableVacancies jobs={jobs} />
-              </article>
-            </div>
-          </section>
+          ) : (
+            <>
+              <div className={styles.infoContainer}>
+                <InfoCard jobs={jobs} users={users} companies={companies} />
+                <RevenueChart
+                  users={users}
+                  className={styles.mercadopagoChart}
+                />
+              </div>
+              <div className={styles.containerGraphAcc}>
+                <div className={styles.containerGraph}>
+                  <BarcharCompany
+                    jobs={jobs}
+                    users={users}
+                    companies={companies}
+                  />
+                  <Piechar jobs={jobs} users={users} companies={companies} />
+                  <CountryPieChart users={users} />
+                </div>
+                <div>
+                  <input
+                    type="text"
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                    placeholder="Search..."
+                    className={styles.buscador}
+                  />
+                  <section className={styles.ac_container}>
+                    <div>
+                      <input
+                        id="ac-1"
+                        name="accordion-1"
+                        type="radio"
+                        checked={accordionState.ac1}
+                        onChange={() => handleAccordionChange("ac1")}
+                      />
+                      <label htmlFor="ac-1">Users Table</label>
+                      <article
+                        className={`${styles.ac_small} ${
+                          accordionState.ac1 ? styles.active : ""
+                        }`}
+                      >
+                        <TableUsers users={filteredUsers} />
+                      </article>
+                    </div>
+                    <div>
+                      <input
+                        id="ac-2"
+                        name="accordion-1"
+                        type="radio"
+                        checked={accordionState.ac2}
+                        onChange={() => handleAccordionChange("ac2")}
+                      />
+                      <label htmlFor="ac-2">Companies Table</label>
+                      <article
+                        className={`${styles.ac_medium} ${
+                          accordionState.ac2 ? styles.active : ""
+                        }`}
+                      >
+                        <TableCompanies companies={filteredCompanies} />
+                      </article>
+                    </div>
+                    <div>
+                      <input
+                        id="ac-3"
+                        name="accordion-1"
+                        type="radio"
+                        checked={accordionState.ac3}
+                        onChange={() => handleAccordionChange("ac3")}
+                      />
+                      <label htmlFor="ac-3">Vacancies Table</label>
+                      <article
+                        className={`${styles.ac_large} ${
+                          accordionState.ac3 ? styles.active : ""
+                        }`}
+                      >
+                        <TableVacancies jobs={jobs} />
+                      </article>
+                    </div>
+                  </section>
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
