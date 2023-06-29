3acd23091944be8c4fd255660e9b719575c6797d
iairkap <iairkap@gmail.com>
2023-06-29 16:47:01 -0300

    NO ANDA

--- src/app/profile/superAdmin/DashSuperadmin/Graficos/TableVacancies/table.jsx
+++ src/app/profile/superAdmin/DashSuperadmin/Graficos/TableVacancies/table.jsx
@@ -12,6 +12,8 @@ function TableVacancies({ jobs }) {
   });
   const [currentPage, setCurrentPage] = useState(1);
   const [usersPerPage] = useState(8);
+  const [editingId, setEditingId] = useState(null);
+  const [editingData, setEditingData] = useState({});
 
   const [jobsData, setJobsData] = useState(jobs);
 
@@ -26,6 +28,11 @@ function TableVacancies({ jobs }) {
       return updatedJobs;
     });
   };
+  const handleEditClick = (job) => {
+    setEditingId(job.id);
+    setEditingData(job);
+    console.log(job); // Add this line
+  };
 
   async function toggleActive(vacancyId, currentStatus) {
     try {
@@ -38,6 +45,22 @@ function TableVacancies({ jobs }) {
       console.error(error);
     }
   }
+  const handleInputChange = (event, field) => {
+    setEditingData((prevData) => ({
+      ...prevData,
+      [field]: event.target.value,
+    }));
+  };
+
+  const handleSaveChanges = async (id) => {
+    try {
+      const response = await axios.patch(`/api/vacancies/${id}`, editingData);
+      setEditingId(null);
+      updateJobStatus(id, editingData.isActive);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const sortedJobs = Object.values(jobsData).sort((a, b) => {
     if (a[sortConfig.key] < b[sortConfig.key]) {
@@ -97,26 +120,89 @@ function TableVacancies({ jobs }) {
               Salary
             </th>
             <th>Activate/Deactivate Job</th>
+            <th>Edit Job</th>
           </tr>
         </thead>
         <tbody>
           {currentJobs.map((job) => (
             <tr key={job.id} className={styles.tr}>
-              <td className={styles.td}>{job.name_Vacancy}</td>
-              <td className={styles.td}>
-                {job.programming_Languages.join(", ")}
-              </td>
-              <td className={styles.td}>{job.seniority}</td>
-              <td className={styles.td}>{job.years_of_experience}</td>
-              <td className={styles.td}>{job.salary}</td>
-              <td className={styles.td}>
-                <button
-                  onClick={() => toggleActive(job.id, job.isActive)}
-                  className={styles.buttonActivation}
-                >
-                  {job.isActive ? "Deactivate" : "Activate"}
-                </button>
-              </td>
+              {editingId === job.id ? (
+                <>
+                  <td className={styles.td}>
+                    <input
+                      style={{ width: "100%", height: "100%" }}
+                      className={styles.input}
+                      type="text"
+                      value={editingData.name_Vacancy}
+                      onChange={(event) =>
+                        handleInputChange(event, "name_Vacancy")
+                      }
+                    />
+                  </td>
+                  <td className={styles.td}>
+                    <input
+                      className={styles.input}
+                      type="text"
+                      value={editingData.programming_Languages}
+                      onChange={(event) =>
+                        handleInputChange(event, "programming_Languages")
+                      }
+                    />
+                  </td>
+                  <td className={styles.td}>
+                    <input
+                      className={styles.input}
+                      type="text"
+                      value={editingData.seniority}
+                      onChange={(event) =>
+                        handleInputChange(event, "seniority")
+                      }
+                    />
+                  </td>
+                  <td className={styles.td}>
+                    <input
+                      className={styles.input}
+                      type="text"
+                      value={editingData.years_of_experience}
+                      onChange={(event) =>
+                        handleInputChange(event, "years_of_experience")
+                      }
+                    />
+                  </td>
+                  <td className={styles.td}>
+                    <input
+                      className={styles.input}
+                      type="text"
+                      value={editingData.salary}
+                      onChange={(event) => handleInputChange(event, "salary")}
+                    />
+                  </td>
+
+                  <td className={styles.td}>
+                    <button onClick={() => handleSaveChanges(job.id)}>
+                      Save
+                    </button>
+                    <button onClick={() => setEditingId(null)}>Cancel</button>
+                  </td>
+                </>
+              ) : (
+                <>
+                  <td className={styles.td}>{job.name_Vacancy}</td>
+                  <td className={styles.td}>{job.programming_Languages}</td>
+                  <td className={styles.td}>{job.seniority}</td>
+                  <td className={styles.td}>{job.years_of_experience}</td>
+                  <td className={styles.td}>{job.salary}</td>
+                  <td className={styles.td}>
+                    <button onClick={() => handleEditClick(job)}>Edit</button>
+                    <button
+                      onClick={() => toggleActive(job.id, job.isActive)}
+                      className={styles.buttonActivation}
+                    >
+                      {job.isActive ? "Deactivate" : "Activate"}
+                    </button>
+                  </td>
+                </>
+              )}
             </tr>
           ))}
         </tbody>
